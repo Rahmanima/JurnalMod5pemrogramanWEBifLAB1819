da5708a68fde5e0d076e3bb3f03e43b7ca6edeb0
Rama Aditya Maulana <ramaadtym@gmail.com>
2019-02-05 10:56:15 +0700

    Update, cleaning Main_NIM.js and data_NIM.js

---
+++ js/main_NIM.js
@@ -0,0 +1,31 @@
+/* 
+    Make sure that each id or class is accompanied by your student id
+    for example #id_1301171234 , .class_1301171234
+
+    FOR QUESTION No. 2, PLEASE USE $.getJSON function to load the data
+    FOR QUESTION No. 3, PLEASE USE $.ajax function to load data
+*/
+
+/*-------------------------------------
+    PUT YOUR getJSON function here
+---------------------------------------*/
+
+
+/*-------------------------------------
+    End of getJSON Function
+---------------------------------------*/
+
+
+
+
+$(document).ready(function(){
+
+/*-------------------------------------
+    PUT YOUR $.ajax function here
+---------------------------------------*/
+    
+
+/*-------------------------------------
+    End Of $.ajax function
+---------------------------------------*/
+})
